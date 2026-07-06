48fe0bfedb92f34ce542f060cdcf55e801d8eaef
Kadir Egilmez <40733099+laperla11@users.noreply.github.com>
2026-07-06 22:13:37 +0100

    Refactor ESLint configuration for JavaScript and TypeScript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,128 +1,94 @@
-/**
- * ESLint configuration for Enterprise React 19.2.6 + TypeScript 5.8.3 hybrid JS/TS project
- * ESLint version: 8.57.1
- */
 module.exports = {
   root: true,
+
   env: {
     browser: true,
-    es2021: true,
-    node: true,
+    es2022: true,
     jest: true,
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'react-hooks',
-    'jsx-a11y',
-    'import',
-    'unused-imports',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-  ],
+
   settings: {
     react: {
       version: 'detect',
     },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      typescript: {},
-    },
   },
-  rules: {
-    // General
-    'no-console': 'warn',
-    'no-debugger': 'error',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
-    ],
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          ['parent', 'sibling', 'index'],
-        ],
-        'newlines-between': 'always',
-        alphabetize: { order: 'asc', caseInsensitive: true },
-      },
-    ],
-    'import/no-unresolved': 'error',
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: ['**/*.test.{js,jsx,ts,tsx}', '**/setupTests.{js,ts}'] },
-    ],
-    // React
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/jsx-uses-vars': 'error',
-    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-    // React Hooks
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    // Accessibility
-    'jsx-a11y/anchor-is-valid': 'warn',
-  },
+
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'coverage/',
+    'build/',
+  ],
+
   overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
-      rules: {
-        'no-undef': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@typescript-eslint/ban-ts-comment': 'warn',
-        '@typescript-eslint/no-floating-promises': 'error',
-        '@typescript-eslint/consistent-type-imports': 'error',
-        '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-      },
-    },
+    // --------------------------
+    // JavaScript
+    // --------------------------
     {
       files: ['**/*.js', '**/*.jsx'],
+
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+      ],
+
       rules: {
-        '@typescript-eslint/no-var-requires': 'off',
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'no-multiple-empty-lines': 'error',
       },
     },
+
+    // --------------------------
+    // TypeScript
+    // --------------------------
     {
-      files: ['*.test.{js,jsx,ts,tsx}', '**/setupTests.{js,ts}'],
-      env: {
-        jest: true,
+      files: ['**/*.ts', '**/*.tsx'],
+
+      parser: '@typescript-eslint/parser',
+
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
       },
+
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+
       rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'no-multiple-empty-lines': 'error',
+      },
+    },
+
+    // --------------------------
+    // Tests
+    // --------------------------
+    {
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/*.test.ts',
+        '**/*.test.tsx',
+      ],
+
+      rules: {
+        '@typescript-eslint/require-await': 'off',
       },
     },
   ],
